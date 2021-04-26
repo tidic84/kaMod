43d2023047aed064ea5113624487efe5b6e94879
tidic84 <tidic84@gmail.com>
2021-04-26 20:51:06 +0200

    Bug Fix: Crash au lancement : Mauvaise direction de la variable CLIENT_PROXY

--- src/main/java/fr/kallium/kamod/References.java
+++ src/main/java/fr/kallium/kamod/References.java
@@ -4,9 +4,9 @@ public class References {
 	
 	public static final String MODID = "kallium";
 	public static final String NAME = "Kallium";
-	public static final String VERSION = "0.6";
+	public static final String VERSION = "0.9";
 	public static final String MC_VERSION = "[1.12]";
-	public static final String CLIENT_PROXY = "fr.tidic.kallium.proxy.ClientProxy";
-	public static final String SERVER_PROXY = "fr.tidic.kallium.proxy.ServerProxy";
+	public static final String CLIENT_PROXY = "fr.kallium.kamod.proxy.ClientProxy";
+	public static final String SERVER_PROXY = "fr.kallium.proxy.kamod.ServerProxy";
 
 }
