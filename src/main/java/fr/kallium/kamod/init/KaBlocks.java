3b429b0e7603a471265fa77cb510016290420546
Stephanie <Stephanie@Port-benoist.idyl.local>
2021-04-27 17:59:31 +0200

    Update Uranium ORe

--- src/main/java/fr/kallium/kamod/init/KaBlocks.java
+++ src/main/java/fr/kallium/kamod/init/KaBlocks.java
@@ -22,14 +22,18 @@ public class KaBlocks {
 		public static final Block bismuth_block = new KaBlock("bismuth_block", Material.ROCK, 5, CreativeTabs.BUILDING_BLOCKS);
 		public static final Block galene_block = new KaBlock("galene_block", Material.ROCK, 5, CreativeTabs.BUILDING_BLOCKS);
 		public static final Block atlas_block = new KaBlock("atlas_block", Material.ROCK, 5, CreativeTabs.BUILDING_BLOCKS);
-		public static final Block chelou_block = new KaBlock("chelou_block", Material.ROCK, 5, CreativeTabs.BUILDING_BLOCKS);
+
 		
 		//Minerais
 		public static final Block kallium_ore = new KaOre("kallium_ore", 3, CreativeTabs.BUILDING_BLOCKS);
 		public static final Block argonite_ore = new KaOre("argonite_ore", 3, CreativeTabs.BUILDING_BLOCKS);
 		public static final Block bismuth_ore = new KaOre("bismuth_ore", 3, CreativeTabs.BUILDING_BLOCKS);
 		public static final Block galene_ore = new KaOre("galene_ore", 3, CreativeTabs.BUILDING_BLOCKS);
+		public static final Block uranium_ore = new KaOre("uranium_ore", 3, CreativeTabs.BUILDING_BLOCKS);
+
 		
 		//Autres
+		public static final Block chelou_block = new KaBlock("chelou_block", Material.ROCK, 5, CreativeTabs.BUILDING_BLOCKS);
+		public static final Block leather_block = new KaBlock("leather_block", Material.ANVIL, 3, CreativeTabs.BUILDING_BLOCKS);
 
 }
