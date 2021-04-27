826d710ce89f579831802a0ac2122be07bf84923
tidic84 <tidic84@gmail.com>
2021-04-27 12:57:31 +0200

    Fix Bug après optimisation

--- src/main/java/fr/kallium/kamod/items/KaItemFood.java
+++ src/main/java/fr/kallium/kamod/items/KaItemFood.java
@@ -1,9 +1,12 @@
 package fr.kallium.kamod.items;
 
+import fr.kallium.kamod.Kallium;
+import fr.kallium.kamod.init.kaItems;
+import fr.kallium.kamod.util.interfaces.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemFood;
 
-public class KaItemFood extends ItemFood {
+public class KaItemFood extends ItemFood implements IHasModel {
 
 	public KaItemFood( String name, CreativeTabs tab, int amount, float saturation,boolean isWolfFood) {
 		super(amount, isWolfFood);
@@ -11,8 +14,14 @@ public class KaItemFood extends ItemFood {
 		setRegistryName(name);
 		setCreativeTab(tab);
 		
+		kaItems.ITEMS.add(this);
 	}
 
-	
+	@Override
+	public void registerModels() {
+		
+		Kallium.proxy.registerItemRenderer(this, 0);
+	}
 
 }
+
--- src/main/java/fr/kallium/kamod/items/KaSwitchMorpher.java
+++ src/main/java/fr/kallium/kamod/items/KaSwitchMorpher.java
@@ -1,18 +1,16 @@
 package fr.kallium.kamod.items;
 
+import fr.kallium.kamod.Kallium;
+import fr.kallium.kamod.init.kaItems;
 import fr.kallium.kamod.playerHelper.KaPlayerHelper;
+import fr.kallium.kamod.util.interfaces.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
-public class KaSwitchMorpher extends Item{
+public class KaSwitchMorpher extends Item implements IHasModel{
 	
 	
 	public KaSwitchMorpher(String name, CreativeTabs tab) {
@@ -20,18 +18,26 @@ public class KaSwitchMorpher extends Item{
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(tab);
+		kaItems.ITEMS.add(this);
+
 	}
 	
+	@Override
+	public void registerModels() {
+		
+		Kallium.proxy.registerItemRenderer(this, 0);
+	}
+
 	@SubscribeEvent
 	public static void TickEvent(PlayerTickEvent player, EntityPlayer playerIn) {
     	
-		{if(player != null) {
+		if(player != null) {
 			KaPlayerHelper.setPlayerSize(playerIn, 0.3F, 0.3F, 0.5F);
 		}
-    	}
+    	
 	/**ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn){
 		KaPlayerHelper.setPlayerSize(playerIn, 0.6F / 2, 1.8F / 2, playerIn.getDefaultEyeHeight() / 2);
         return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
-    }*/
+    }**/
 }
 }
