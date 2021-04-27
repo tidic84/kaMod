3a2c27bbf77b426fb282e56fc77248dc9578cdf9
tidic84 <tidic84@gmail.com>
2021-04-27 21:03:26 +0200

    Ajout Décraft des blocks de minerais

--- src/main/java/fr/kallium/kamod/init/KaRecipes.java
+++ src/main/java/fr/kallium/kamod/init/KaRecipes.java
@@ -12,14 +12,8 @@ public class KaRecipes {
 	
 	public static void init() {
 		GameRegistry.addSmelting(Items.ROTTEN_FLESH, new ItemStack(kaItems.purified_flesh), 2);
-		GameRegistry.addSmelting(new ItemStack(KaBlocks.kallium_ore), new ItemStack(Blocks.STONE), 5);
-		GameRegistry.addSmelting(new ItemStack(Items.AIR), new ItemStack(KaBlocks.argonite_ore, 1), 5);
-		GameRegistry.addSmelting(Items.APPLE, new ItemStack(kaItems.kallium), 5);
-		GameRegistry.addSmelting(new ItemStack(kaItems.galene), new ItemStack(KaBlocks.galene_ore), 5);
-		GameRegistry.addSmelting(Blocks.STONE, new ItemStack(Items.APPLE, 4), 10f);	
-		GameRegistry.addSmelting(kaItems.atlas, new ItemStack(Items.APPLE, 5), 5);;	
-		GameRegistry.addSmelting(Blocks.BED, new ItemStack(kaItems.atlas, 5), 5);;	
-		GameRegistry.addSmelting(KaBlocks.argonite_block, new ItemStack(Items.APPLE), 4);
+		GameRegistry.addSmelting(new ItemStack(KaBlocks.argonite_ore), new ItemStack(kaItems.argonite), 2);
+		
 
 	}
 }
