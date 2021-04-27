5351aa2d9d74dec44c2f6f8caaad4b6d52a16a24
tidic84 <tidic84@gmail.com>
2021-04-27 17:20:41 +0200

    Revert "Fix Bug après optimisation"
    This reverts commit 826d710ce89f579831802a0ac2122be07bf84923.

--- src/main/java/fr/kallium/kamod/items/KaItemFood.java
+++ src/main/java/fr/kallium/kamod/items/KaItemFood.java
@@ -1,12 +1,9 @@
 package fr.kallium.kamod.items;
 
-import fr.kallium.kamod.Kallium;
-import fr.kallium.kamod.init.kaItems;
-import fr.kallium.kamod.util.interfaces.IHasModel;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemFood;
 
-public class KaItemFood extends ItemFood implements IHasModel {
+public class KaItemFood extends ItemFood {
 
 	public KaItemFood( String name, CreativeTabs tab, int amount, float saturation,boolean isWolfFood) {
 		super(amount, isWolfFood);
@@ -14,14 +11,8 @@ public class KaItemFood extends ItemFood implements IHasModel {
 		setRegistryName(name);
 		setCreativeTab(tab);
 		
-		kaItems.ITEMS.add(this);
 	}
 
-	@Override
-	public void registerModels() {
-		
-		Kallium.proxy.registerItemRenderer(this, 0);
-	}
+	
 
 }
-
