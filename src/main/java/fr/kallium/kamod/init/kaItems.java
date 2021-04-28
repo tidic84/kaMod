6782fdc0942c87938eebe7019d2700a14292c03a
tidic84 <tidic84@gmail.com>
2021-04-28 12:25:13 +0200

    Ajouts Armures: Argonite, Bismuth, Galene

--- src/main/java/fr/kallium/kamod/init/kaItems.java
+++ src/main/java/fr/kallium/kamod/init/kaItems.java
@@ -23,9 +23,9 @@ public class kaItems {
 	
 	//Armor Material
 	public static final ArmorMaterial kallium_armor = EnumHelper.addArmorMaterial("kallium_armor", ":kallium", 100, new int[] {15, 21, 20, 18}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
-	public static final ArmorMaterial argonite_armor = EnumHelper.addArmorMaterial("argonite_armor", ":kallium", 100, new int[] {15, 21, 20, 18}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
-	public static final ArmorMaterial bismuth_armor = EnumHelper.addArmorMaterial("bismuth_armor", ":kallium", 100, new int[] {15, 21, 20, 18}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
-	public static final ArmorMaterial galene_armor = EnumHelper.addArmorMaterial("galene_armor", ":kallium", 100, new int[] {15, 21, 20, 18}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
+	public static final ArmorMaterial argonite_armor = EnumHelper.addArmorMaterial("argonite_armor", ":kallium", 100, new int[] {10, 15, 14, 13}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
+	public static final ArmorMaterial bismuth_armor = EnumHelper.addArmorMaterial("bismuth_armor", ":kallium", 100, new int[] {10, 15, 14, 13}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
+	public static final ArmorMaterial galene_armor = EnumHelper.addArmorMaterial("galene_armor", ":kallium", 100, new int[] {10, 15, 14, 13}, 18, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 1.0F);
 	
 	//Kallium
 	public static final Item kallium = new kaItem("kallium", CreativeTabs.MISC);
@@ -36,13 +36,25 @@ public class kaItems {
 	
 	//Argoite
 	public static final Item argonite = new kaItem("argonite", CreativeTabs.MISC);
+	public static final Item argonite_helmet = new KaArmor("argonite_helmet", argonite_armor, 1, EntityEquipmentSlot.HEAD);
+	public static final Item argonite_chestplate = new KaArmor("argonite_chestplate", argonite_armor, 1, EntityEquipmentSlot.CHEST);
+	public static final Item argonite_leggings = new KaArmor("argonite_leggings", argonite_armor, 2, EntityEquipmentSlot.LEGS);
+	public static final Item argonite_boots = new KaArmor("argonite_boots", argonite_armor, 1, EntityEquipmentSlot.FEET);
 	
 	//Bismuth
 	public static final Item bismuth = new kaItem("bismuth", CreativeTabs.MISC);
-		
+	public static final Item bismuth_helmet = new KaArmor("bismuth_helmet", bismuth_armor, 1, EntityEquipmentSlot.HEAD);
+	public static final Item bismuth_chestplate = new KaArmor("bismuth_chestplate", bismuth_armor, 1, EntityEquipmentSlot.CHEST);
+	public static final Item bismuth_leggings = new KaArmor("bismuth_leggings", bismuth_armor, 2, EntityEquipmentSlot.LEGS);
+	public static final Item bismuth_boots = new KaArmor("bismuth_boots", bismuth_armor, 1, EntityEquipmentSlot.FEET);
+	
 	//Galene
 	public static final Item galene = new kaItem("galene", CreativeTabs.MISC);
-		
+	public static final Item galene_helmet = new KaArmor("galene_helmet", galene_armor, 1, EntityEquipmentSlot.HEAD);
+	public static final Item galene_chestplate = new KaArmor("galene_chestplate", galene_armor, 1, EntityEquipmentSlot.CHEST);
+	public static final Item galene_leggings = new KaArmor("galene_leggings", galene_armor, 2, EntityEquipmentSlot.LEGS);
+	public static final Item galene_boots = new KaArmor("galene_boots", galene_armor, 1, EntityEquipmentSlot.FEET);
+	
 	//Atlas
 	public static final Item atlas = new kaItem("atlas", CreativeTabs.MISC);
 		
